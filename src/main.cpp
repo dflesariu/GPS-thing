1d336a45e498fdc0e96e521600036221889e61d7
dflesariu <flesariudragos@gmail.com>
2024-06-26 23:07:24 +0100

    Updated TODO

--- src/main.cpp
+++ src/main.cpp
@@ -197,15 +197,15 @@ int historyMenu() {
   tft.drawRect(0, 0, tft.width(), tft.height(), TFT_BLUE);
 
   // Set "cursor" at top left corner of display (0,0) and select font
-  tft.setCursor(10, 10, 2);
+  tft.setCursor((tft.height()*5/100), (tft.height()*10/100), 2);
   // Print the name of the menu entry
   tft.println("History");
   // Draw a line 
-  tft.drawLine(0, 30, tft.width(), 30, TFT_BLUE);
+  tft.drawLine(0, (tft.height()*25/100), tft.width(), (tft.height()*25/100), TFT_BLUE);
 
   //Back to main Menu
-  tft.drawLine(0, 125, tft.width(), 125, TFT_BLUE);
-  tft.setCursor(4, 137, 2);
+  tft.drawLine(0, (tft.height()*75/100), tft.width(), (tft.height()*75/100), TFT_BLUE);
+  tft.setCursor((tft.height()*5/100), (tft.height()*85/100), 2);
   tft.println("Main Menu");
 
   // Read the button state with delay, necessary
